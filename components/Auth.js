6a9517b7cd9841c0655249fce15847030f757caf
Dhruv P <privatesigma01@gmail.com>
2025-09-12 14:32:13 +0530

    Create Auth.js

---
+++ components/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Login / Signup Page</h2>
+    </div>
+  );
+};
+
+export default Auth;
