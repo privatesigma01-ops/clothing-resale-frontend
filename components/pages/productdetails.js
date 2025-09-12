1e7dcf718fbf9796a9f2de7060361c27606bab8a
Dhruv P <privatesigma01@gmail.com>
2025-09-12 14:33:26 +0530

    Create productdetails.js

---
+++ components/pages/productdetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetails = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Product Details</h2>
+    </div>
+  );
+};
+
+export default ProductDetails;
