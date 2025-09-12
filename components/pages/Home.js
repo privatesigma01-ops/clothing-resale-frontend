beb80614acefd2828d4dd834054d9db725660fe5
Dhruv P <privatesigma01@gmail.com>
2025-09-12 14:32:54 +0530

    Create Home.js

---
+++ components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Welcome to the Clothing Resale Store</h2>
+      <p>Browse items and resell easily!</p>
+    </div>
+  );
+};
+
+export default Home;
